4c5daa1d7d9e048b10d798bff0c88bec19bfb8db
Laurence Newman <lenewman05@gmail.com>
2017-02-04 10:45:45 +0000

    spa

--- src/app/components/landing.js
+++ src/app/components/landing.js
@@ -3,7 +3,7 @@ import Department from './department';
 
 const Landing = props => (
   <div>
-    <Department subject="Biomedical" description="bla bla bla.." link="/quiz" />
+    <Department subject="Biomedical" description="bla bla bla.." link="/#quiz" />
     <Department subject="Chemistry" description="bla bla bla.." link="external" />
     <Department subject="Life sciences" description="bla bla bla.." link="external" />
     <Department subject="Physics" description="bla bla bla.." link="external" />
--- src/app/containers/root.js
+++ src/app/containers/root.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Router, Route, browserHistory, IndexRedirect } from "react-router";
+import { Router, Route, hashHistory, IndexRoute } from "react-router";
 import { Provider } from "react-redux";
 import Landing from "../components/landing";
 import App from "../components/app";
@@ -7,9 +7,9 @@ import Quiz from "./quiz";
 
 const Root = ({ store }) => (
   <Provider store={store}>
-    <Router history={browserHistory} >
+    <Router history={hashHistory} >
       <Route path="/" component={App} >
-        <IndexRedirect to="home" />
+        <IndexRoute component={Landing} />
         <Route path="quiz" component={Quiz} />
         <Route path="*" component={Landing} />
       </Route>
